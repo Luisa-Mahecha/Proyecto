f487156d80a0eb6a1b43d56eca467758bb47076a
Luisa-Mahecha <luimahedel@gmail.com>
2025-10-15 22:27:28 -0500

    Primer commit

---
+++ Backend/db.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2");
+
+
+const db = mysql.createConnection({
+  host: "localhost",   
+  user: "root",        
+  password: "2005123", 
+  database: "Proyecto" 
+});
+
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error al conectar a MySQL:", err);
+    return;
+  }
+  console.log("Conectado a la base de datos MySQL ✅");
+});
+
+module.exports = db;
